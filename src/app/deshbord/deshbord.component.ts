c3698b1b84a89d9cc76e060d769f1b26d1aef423
maroka <cedric.maroka@reverside.co.za>
2018-10-14 16:58:42 +0200

    validation added by cedric

--- src/app/deshbord/deshbord.component.ts
+++ src/app/deshbord/deshbord.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-deshbord',
@@ -7,25 +8,52 @@ import { DOCUMENT } from '@angular/platform-browser';
   styleUrls: ['./deshbord.component.css']
 })
 export class DeshbordComponent implements OnInit {
-  name:any;
-  manu=[];
-  cartData:any={itemNum:"5",value:"My Cart"};
-  public fixed: boolean = false; 
+  name: any;
+  manu = [];
+  hideManu: boolean = true;
+  searchBusy: boolean = false;
+  manuOpen: boolean = false;
+  cartData: any = { itemNum: "5", value: "My Cart" };
+  public fixed: boolean = false;
+  searchForm: any;
 
-         constructor(@Inject(DOCUMENT) private doc: Document) {}
+  constructor(@Inject(DOCUMENT) private doc: Document, private formBuilder: FormBuilder, ) { }
 
-         @HostListener("window:scroll", [])
-         onWindowScroll() {
-            let num = this.doc.body.scrollTop;
-            if ( num > 50 ) {
-                this.fixed = true;
-            }else if (this.fixed && num < 5) {
-                this.fixed = false;
-            }
-          }
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    let num = this.doc.body.scrollTop;
+    if (num > 50) {
+      this.fixed = true;
+    } else if (this.fixed && num < 5) {
+      this.fixed = false;
+    }
+  }
   ngOnInit() {
-    this.name="Cedric";
-    this.manu.push({value:"surname"},{value:"surname"},{value:"surname"},{value:"surname"})
+    this.name = "Cedric";
+    this.manu.push({ value: "surname" }, { value: "surname" }, { value: "surname" }, { value: "surname" })
+    this.formBuilder = new FormBuilder();
+    this.searchForm = this.formBuilder.group({
+      search: new FormControl('', {}),
+    })
+
+  }
+  toggleManu() {
+    this.hideManu = !this.hideManu;
+    this.manuOpen = !this.manuOpen;
+  }
+  clicItem() {
+    this.hideManu = !this.hideManu;
+    this.manuOpen = !this.manuOpen;
+  }
+  search(e) {
+    if (e.target.value.trim() !== '')
+      this.searchBusy = true;
+    else
+      this.searchBusy = false;
   }
 
+  clearSerch() {
+    this.searchForm.controls['search'].setValue('');
+    this.searchBusy = false;
+  }
 }
